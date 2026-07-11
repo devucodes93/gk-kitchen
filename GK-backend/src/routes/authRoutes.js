980983494cdaa3083717608b457007aff5ad1e3d
Mounesh V <mouneshv696@gmail.com>
2026-07-11 18:44:25 +0530

    Removed unwanted things

--- GK-backend/src/routes/authRoutes.js
+++ GK-backend/src/routes/authRoutes.js
@@ -16,7 +16,7 @@ router.get(
   "/google/callback",
   passport.authenticate("google", {
     session: false,
-    failureRedirect: "http://localhost:3000",
+    failureRedirect: "https://gk-kitchen-blush.vercel.app/",
   }),
   (req, res) => {
     const token = jwt.sign(
