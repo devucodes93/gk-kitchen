2a179853ba25cf2e15a0f1459eda1a9b0804ea1c
Mounesh V <mouneshv696@gmail.com>
2026-07-18 10:49:26 +0530

    Payment Integration

--- GK-backend/src/routes/paymentRoutes.js
+++ GK-backend/src/routes/paymentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { createPaymentOrder, verifyPayment } = require("../controller/paymentController");
+const { createPaymentOrder, verifyPayment } = require("../controller/paymentController.js");
 const protect = require("../middleware/authMiddleware");
 
 router.post("/create-order", protect, createPaymentOrder);
